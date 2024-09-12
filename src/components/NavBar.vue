8c67ec4f09136b26c50647a3ad0bec06e31d8b10
Ghassan Al Karaan <ghass.karaan@gmail.com>
2024-09-12 17:53:53 +0300

    prepared project

---
+++ src/components/NavBar.vue
@@ -0,0 +1,74 @@
+<script setup>
+
+import { RouterLink, useRoute } from "vue-router";
+import logo from "@/assets/logo.svg";
+
+const isActiveLink = (routePath) => {
+    const route = useRoute();
+    return route.path === routePath;
+}
+
+</script>
+
+<template>
+
+    <nav>
+        <RouterLink to="/">
+            <div class="logo">
+                <img :src="logo" alt="Logo" />
+            </div>
+        </RouterLink>
+        <div class="links">
+            <ul>
+                <RouterLink to="/" :class="{ active: isActiveLink('/') }">
+                    <li>Home</li>
+                </RouterLink>
+                <RouterLink to="/404" :class="{ active: isActiveLink('/404') }">
+                    <li>Not Home</li>
+                </RouterLink>
+            </ul>
+        </div>
+    </nav>
+
+</template>
+
+<style scoped>
+.logo {
+    height: 40px;
+    width: 40px;
+}
+
+nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+ul {
+    display: flex;
+    gap: 1rem;
+    list-style: none;
+}
+
+li {
+    font-size: 18px;
+}
+
+a {
+    background: none;
+}
+
+.active {
+    color: white;
+}
+
+a:hover {
+    color: white;
+}
+
+@media (max-width: 480px) {
+    .logo {
+        display: none;
+    }
+}
+</style>
